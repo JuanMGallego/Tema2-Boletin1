ff2cb5588feea20041bdf6b0fa5e58f5d2f86f9d
jmgallego <jmgallego@106-09.info2>
2022-10-05 14:28:48 +0200

    First commit

--- Tema2-Boletin1/src/tema2boletin1/Exercise1.java
+++ Tema2-Boletin1/src/tema2boletin1/Exercise1.java
@@ -1,10 +1,37 @@
 package tema2boletin1;
 
+import java.util.Scanner;
+
 public class Exercise1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int num; //Variable declared and used to let the user introduce the number
+		
+		Scanner sc = new Scanner(System.in); //Scanner is started to introduce data into variables
+		
+		/*Prints a line to inform the user where he/she has to put the number, 
+		then Scanner asks for it, so the scanner opens a space to let the user introduce it*/
+		
+		System.out.print("Introduzca un número: ");
+		num = sc.nextInt();
+		System.out.println();
+		
+		/*If the number is par, it will be printed, else, the program will print that it isn't*/
+		
+		if (num % 2 == 0) {
+		
+			System.out.println("El número introducido es par");
+		
+		} else {
+			
+			System.out.println("El número introducido no es par");
+			
+		}
+		
+		sc.close(); //Scanner is closed
+		
 	}
 
 }
---
+++ Tema2-Boletin1/src/tema2boletin1/Exercise2.java
@@ -0,0 +1,41 @@
+package tema2boletin1;
+
+import java.util.Scanner;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1; //Variable declared and used to let the user introduce the number
+		int num2;
+		
+		Scanner sc = new Scanner(System.in); //Scanner is started to introduce data into variables
+		
+		/*Prints a line to inform the user where he/she has to put the numbers, 
+		then Scanner asks for them, so the scanner opens a space to let the user introduce it*/
+		
+		System.out.print("Introduzca un número: ");
+		num1 = sc.nextInt();
+		
+		System.out.print("Introduzca otro número: ");
+		num2 = sc.nextInt();
+		System.out.println();
+		
+		/*If the numbers are the same, it will be printed, else, the program will print that they aren't*/
+		
+		if (num1 == num2) {
+		
+			System.out.println("Los números introducidos son iguales");
+		
+		} else {
+			
+			System.out.println("Los números introducidos no son iguales");
+			
+		}
+		
+		sc.close(); //Scanner is closed
+		
+	}
+
+}
